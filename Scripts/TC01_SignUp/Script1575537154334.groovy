e814ec6097963a2a55497caf5078b84f87b17463
Bhawna Pathak <Bhawna.pathak@xoriant.com>
2019-12-09 16:52:06 +0530

    added try catch finally

--- Scripts/TC01_SignUp/Script1575537154334.groovy
+++ Scripts/TC01_SignUp/Script1575537154334.groovy
@@ -13,37 +13,49 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.openBrowser('')
+try {
+	WebUI.openBrowser('')
+	
+	WebUI.navigateToUrl(GlobalVariable.url)
+	
+	WebUI.maximizeWindow()
+	
+	WebUI.verifyTextPresent("PHPTRAVELS", false)
+	
+	WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account'))
+	WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account (1)'))
+	WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account (1)'))
+	
+	WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_Sign Up'))
+	
+	WebUI.click(findTestObject('Object Repository/Page_Register/input_First Name_firstname'))
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Register/input_First Name_firstname'), 'Bhawna')
+	
+	WebUI.click(findTestObject('Object Repository/Page_Register/input_Last Name_lastname'))
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Register/input_Last Name_lastname'), 'Pathak')
+	
+	WebUI.click(findTestObject('Object Repository/Page_Register/input_Mobile Number_phone'))
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Register/input_Mobile Number_phone'), '123456')
+	
+	WebUI.click(findTestObject('Object Repository/Page_Register/input_Email_email'))
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Register/input_Email_email'), 'abc@gmail.com')
+	
+	WebUI.setEncryptedText(findTestObject('Object Repository/Page_Register/input_Password_password'), 'mdnvewzpQvY=')
+	
+	WebUI.setEncryptedText(findTestObject('Object Repository/Page_Register/input_Confirm Password_confirmpassword'), 'mdnvewzpQvY=')
+	
+	WebUI.click(findTestObject('Object Repository/Page_Register/button_Sign Up'))
+	
+} catch (Exception e) {
+	e.printStackTrace()
+}
 
-WebUI.navigateToUrl(GlobalVariable.url)
-
-WebUI.maximizeWindow()
-
-WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account'))
-WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account (1)'))
-WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_My Account (1)'))
-
-WebUI.click(findTestObject('Object Repository/Page_PHPTRAVELS  Travel Technology Partner/a_Sign Up'))
-
-WebUI.click(findTestObject('Object Repository/Page_Register/input_First Name_firstname'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Register/input_First Name_firstname'), 'Bhawna')
-
-WebUI.click(findTestObject('Object Repository/Page_Register/input_Last Name_lastname'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Register/input_Last Name_lastname'), 'Pathak')
-
-WebUI.click(findTestObject('Object Repository/Page_Register/input_Mobile Number_phone'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Register/input_Mobile Number_phone'), '123456')
-
-WebUI.click(findTestObject('Object Repository/Page_Register/input_Email_email'))
-
-WebUI.setText(findTestObject('Object Repository/Page_Register/input_Email_email'), 'abc@gmail.com')
-
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Register/input_Password_password'), 'mdnvewzpQvY=')
-
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_Register/input_Confirm Password_confirmpassword'), 'mdnvewzpQvY=')
-
-WebUI.click(findTestObject('Object Repository/Page_Register/button_Sign Up'))
+finally{
+	WebUI.closeBrowser(FailureHandling.OPTIONAL)
+}
+	
 
